f42d674cac48b57832ebc2f4e636bb8ccebc2c93
Lain1447 <hardee2@protonmail.com>
2020-01-21 06:03:13 +0000

    Bug with parenthesis fixed and added clear command

--- main.js
+++ main.js
@@ -1,11 +1,15 @@
 const Discord = require('discord.js');
 const client = new Discord.Client();
 
-const token = 'NjYzNDI1MzcyMzgxMTE4NDk0.XiZ4QA.nkv_litaLX6IJeAJGrVyTZ6YwM4';
+const token = 'NjY5MDUwMzU3MjI1MDk1MTg5.XiaL4w.ADo-OGO-5ukKz67Is-JnzslNBBI';
 
 // Prefix for the bot
 const PREFIX = '$';
 
+// Version
+var version = ("0.0.1");
+
+// Main
 client.on('ready', () => {
     console.log('The robot is online!');
 })
@@ -17,27 +21,31 @@ client.on('message', message => {
 })
 
 client.on('message', message => {
+
             let args = message.content.substring(PREFIX.length).split(" ");
+
             switch (args[0]) {
                 case 'ping':
-                    message.reply('yes im here');
+                    message.channel.sendMessage('yes im here');
                     break;
                 case 'help':
-                    message.reply('I only ping rn :(');
+                    message.channel.sendMessage('I only ping rn ');
                     break;
                 case 'source':
-                    message.reply('https://github.com/Mioxoi/discord-bot');
+		    message.channel.sendMessage('https://github.com/Mioxoi/discord-bot');
                     break;
                 case 'info':
-                    if (args[1] === 'version') {
-                        message.channel.sendMessage('Version ' + 'version');
+                    if(args[1] === 'version') {
+                        message.channel.sendMessage('Version ' + version);
                     } else {
                         message.channel.sendMessage('enter a second argument');
                     }
-                    break;
+                break;
                 case 'clear':
                     if (!args[1]) return message.reply('Please enter a second argument');
                     message.channel.bulkDelete(args[1]);
                     break;
+    }
+})
 
-                    client.login(token);
+client.login(token);
