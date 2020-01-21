169c4b8e9806704f233b50133e91a98724e78f8e
Lain1447 <hardee2@protonmail.com>
2020-01-21 07:22:32 +0000

    I'm too lazy to do the rest rn

--- README.md
+++ README.md
@@ -1,1 +1,4 @@
-# discord-bot
+# testBot
+## A fairly useless bot in JS
+###### In all honesty, I'm not sure why I made this.
+It's what it says on the tin, a bot in progress which may start to have its own personality and rely on ML if I decide to stay motivated enough.
--- main.js
+++ main.js
@@ -7,7 +7,7 @@ const token = 'NjY5MDUwMzU3MjI1MDk1MTg5.XiaL4w.ADo-OGO-5ukKz67Is-JnzslNBBI';
 const PREFIX = '$';
 
 // Version
-var version = ("0.0.1");
+var version = ("0.0.2");
 
 // Main
 client.on('ready', () => {
@@ -21,31 +21,37 @@ client.on('message', message => {
 })
 
 client.on('message', message => {
-
-            let args = message.content.substring(PREFIX.length).split(" ");
-
-            switch (args[0]) {
-                case 'ping':
-                    message.channel.sendMessage('yes im here');
-                    break;
-                case 'help':
-                    message.channel.sendMessage('I only ping rn ');
-                    break;
-                case 'source':
-		    message.channel.sendMessage('https://github.com/Mioxoi/discord-bot');
-                    break;
-                case 'info':
-                    if(args[1] === 'version') {
-                        message.channel.sendMessage('Version ' + version);
-                    } else {
-                        message.channel.sendMessage('enter a second argument');
-                    }
+    let args = message.content.substring(PREFIX.length).split(" ");
+    switch (args[0]) {
+        case 'ping':
+            message.channel.sendMessage('yes im here');
+            break;
+        case 'help':
+            message.channel.sendMessage('I only ping rn ');
+            break;
+        case 'source':
+            message.channel.sendMessage('https://github.com/Mioroxi/discord-bot');
+            break;
+        case 'info':
+            if (args[1] === 'version') {
+                message.channel.sendMessage('Version ' + version);
+            } else {
+                message.channel.sendMessage('enter a second argument');
                 break;
-                case 'clear':
-                    if (!args[1]) return message.reply('Please enter a second argument');
-                    message.channel.bulkDelete(args[1]);
-                    break;
-    }
+            }
+	case 'creator':
+	    message.channel.sendMessage("The glorious hardee-sama, hence my questionable programming.");
+            break;
+	case 'clear':
+            if (!args[1]) return message.reply('Please enter a second argument');
+            message.channel.bulkDelete(args[1]);
+            break;
+        case 'fuck you':
+            message.channel.sendMessage('pls no bully. Q_Q');
+            break;
+	}
 })
+
+
 
 client.login(token);
