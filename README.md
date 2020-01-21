b90452ca31ef6d5f3e91666db70eb97d9f3a8d25
Lain1447 <hardee2@protonmail.com>
2020-01-21 08:48:37 +0000

    new commands

--- README.md
+++ README.md
@@ -1,1 +1,4 @@
 # discord-bot
+## A trialing discord bot ig
+
+Just what it says on the tin, nothing too interesting ig.
--- main.js
+++ main.js
@@ -1,7 +1,7 @@
 const Discord = require('discord.js');
 const client = new Discord.Client();
 
-const token = 'NjY5MDUwMzU3MjI1MDk1MTg5.XiaL4w.ADo-OGO-5ukKz67Is-JnzslNBBI';
+const token = 'NjY5MDkyMTI1Mjg3MTIwODk3.XiayzA.h5NKnN_BLZNVtLlGVVgisChAkYo';
 
 // Prefix for the bot
 const PREFIX = '$';
@@ -21,9 +21,7 @@ client.on('message', message => {
 })
 
 client.on('message', message => {
-
             let args = message.content.substring(PREFIX.length).split(" ");
-
             switch (args[0]) {
                 case 'ping':
                     message.channel.sendMessage('yes im here');
@@ -45,6 +43,24 @@ client.on('message', message => {
                     if (!args[1]) return message.reply('Please enter a second argument');
                     message.channel.bulkDelete(args[1]);
                     break;
+		case 'hey bot, fuck you.':
+			message.channel.sendMessage('pls no bully. Q_Q');
+			break;
+	    }
+})
+	client.on('message', message => {
+            let args = message.content.substring(PREFIX.length).split(" ");
+            switch (args[0]) {
+		    case 'embed':
+			    const embed = new Discord.RichEmbed()
+			    .setTitle('User Information')
+			    .addField('Player Name', message.author.username)
+			    .addField('Current Server', message.guild.name)
+			    .setColor('#9CF1F0')
+			    .setThumbnail(message.author.avatarURL)
+			    message.channel.sendEmbed(embed);
+			    break;
+
     }
 })
 
