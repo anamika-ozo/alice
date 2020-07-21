27f566fe35f4ec8738fb6e1c0a1fcccf03d18677
Mioroxi <hana19.nf@protonmail.ch>
2020-07-21 16:07:06 +0100

    aksdlf

--- src/avatars.js
+++ src/avatars.js
@@ -1,5 +1,23 @@
-client.on('message', message => {
-  if (message.content === 'what is my avatar') {
-    message.reply(message.author.displayAvatarURL());
-  }
-});
+var config = require("../static/config.json"),
+    main = require("./main.js"),
+    version = require("../package.json").version,
+    avatars = require("./avatars.js");
+
+var avatar = {
+    "avatar": {
+        desc: "kick a user",
+        usage: "<@user> [message]",
+        deleteCommand: true,
+        cooldown: 3,
+        process: function(client, msg, suffix) {
+            client.on('message', message => {
+                if (message.content === 'what is my avatar') {
+                    message.reply(message.author.displayAvatarURL());
+                }
+            });
+        }
+
+    }
+}
+
+exports = { avatar };
